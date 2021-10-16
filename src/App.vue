e6553f2be5b243df71ffbd853a803e448c1f592c
Junior Alves <junior@boxti.com.br>
2021-10-15 21:10:46 -0300

    feat: Desenvolvido front de dados da covid 19

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,122 @@
+
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row mt-5">
+      <div class="col">
+        <h1 class="text-center">Dados COVID-19 SC</h1>
+      </div>
+    </div>
+    <div class="row mt-5" v-if="arrConfirmed.length > 0">
+      <div class="col">
+        <h2 class="text-center">Positivo</h2>
+        <line-chart
+          :chartData="arrConfirmed"
+          :options="chartOptions"
+          :chartColors="arrConfirmedChartColors"
+          label="Positivo"
+        />
+      </div>
+    </div>
+
+    <div class="row mt-5" v-if="arrEstimatedPopulation.length > 0">
+      <div class="col">
+        <h2 class="text-center">População estimada</h2>
+        <line-chart
+          :chartData="arrEstimatedPopulation"
+          :options="chartOptions"
+          :chartColors="arrEstimatedPopulationChartColors"
+          label="População estimada"
+        />
+      </div>
+    </div>
+
+    <div class="row mt-5" v-if="arrDeaths.length > 0">
+      <div class="col">
+        <h2 class="text-center">Mortes</h2>
+        <line-chart
+          :chartData="arrDeaths"
+          :options="chartOptions"
+          :chartColors="arrDeathsChartColors"
+          label="Mortes"
+        />
+      </div>
+    </div>
+
+    <div class="row mt-5" v-if="arrDeathRate.length > 0">
+      <div class="col">
+        <h2 class="text-center">Com respiradores</h2>
+        <line-chart
+          :chartData="arrDeathRate"
+          :options="chartOptions"
+          :chartColors="arrDeathRateChartColors"
+          label="Com respiradores"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
+import moment from "moment";
+import LineChart from "./components/LineChart.vue";
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    LineChart
+  },
+  data() {
+    return {
+      arrConfirmed: [],
+      arrConfirmedChartColors: {
+        borderColor: "#077187",
+        pointBorderColor: "#0E1428",
+        pointBackgroundColor: "#AFD6AC",
+        backgroundColor: "#74A57F"
+      },
+      arrEstimatedPopulation: [],
+      arrEstimatedPopulationChartColors: {
+        borderColor: "#251F47",
+        pointBorderColor: "#260F26",
+        pointBackgroundColor: "#858EAB",
+        backgroundColor: "#858EAB"
+      },
+      arrDeaths: [],
+      arrDeathsChartColors: {
+        borderColor: "#190B28",
+        pointBorderColor: "#190B28",
+        pointBackgroundColor: "#E55381",
+        backgroundColor: "#E55381"
+      },
+      arrDeathRate: [],
+      arrDeathRateChartColors: {
+        borderColor: "#784F41",
+        pointBorderColor: "#784F41",
+        pointBackgroundColor: "#BBE5ED",
+        backgroundColor: "#BBE5ED"
+      },
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    };
+  },
+  async created() {
+    const { data } = await axios.get("http://localhost:8000/infosCovid/");
+    const results = data.message.results
+    results.forEach(d => {
+      const date = moment(d.date, "YYYYMMDD").format("MM/DD");
+      const {
+        confirmed,
+        estimated_population,
+        deaths,
+        death_rate,
+        city
+      } = d;
+      this.arrConfirmed.push({ date, total: confirmed, cidade: city });
+      this.arrEstimatedPopulation.push({ date, total: estimated_population, cidade: city });
+      this.arrDeaths.push({ date, total: deaths, cidade: city });
+      this.arrDeathRate.push({ date, total: death_rate, cidade: city });
+    });
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/LineChart.vue
@@ -0,0 +1,49 @@
+<script>
+import { Line } from "vue-chartjs";
+export default {
+  extends: Line,
+  props: {
+    label: {
+      type: String
+    },
+    chartData: {
+      type: Array
+    },
+    options: {
+      type: Object
+    },
+    chartColors: {
+      type: Object
+    }
+  },
+  async mounted() {
+    const dates = this.chartData.map(d => d.date).reverse();
+    const totals = this.chartData.map(d => d.total).reverse();
+    const city = this.chartData.map(d => d.cidade).reverse();
+
+    const {
+      borderColor,
+      pointBorderColor,
+      pointBackgroundColor,
+      backgroundColor
+    } = this.chartColors;
+    this.renderChart(
+      {
+        labels: dates,
+        datasets: [
+          {
+            label: this.label,
+            data: totals,
+            cidade: city,
+            borderColor: borderColor,
+            pointBorderColor: pointBorderColor,
+            pointBackgroundColor: pointBackgroundColor,
+            backgroundColor: backgroundColor
+          }
+        ]
+      },
+      this.options
+    );
+  }
+};
+</script>
